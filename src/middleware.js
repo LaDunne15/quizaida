3f9ed4c9b39d7aaf6f4639491c98a1903ccfb83a
LaDunne15 <vladilsav7772@gmail.com>
2024-03-01 23:11:31 +0200

    v.0.3.1 hotfix middlevawe

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,10 @@
 import { NextResponse } from 'next/server'
 import { verifyJwtToken } from './libs/auth';
-import connectDB from './libs/db/mongodb';
  
 export async function middleware(request) {
-
-  //connectDB();
   const token = request.cookies.get('token')?.value;
   const url = request.url;
   const hasVerifiedToken = token && (await verifyJwtToken(token));
-  //console.log(token);
-  //console.log(request.nextUrl.pathname);
   if (!hasVerifiedToken) {
     if(url.includes("auth/login")||url.includes("auth/signup")) {
       return NextResponse.next();
