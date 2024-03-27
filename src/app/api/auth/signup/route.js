b9883933c08ddd2debc2443760d51911d2559a0c
LaDunne15 <vladilsav7772@gmail.com>
2024-03-27 23:18:30 +0200

    hotfix 7?

--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -11,12 +11,16 @@ export async function GET(req) {
         const email = req.nextUrl.searchParams.get("email");
         const user = await User.findOne({ email });
 
-        if (user) return NextResponse.json({}, {
-            status: 400,
+        if (user) return NextResponse.json({
+            success: false
+        }, {
+            status: 201,
             statusText: "This email is already in use by another user"
         });
-        
-        return NextResponse.json({}, {
+
+        return NextResponse.json({
+            success: true
+        }, {
             status: 201,
             statusText: "This email is free"
         });
